9f481d40c66c130e8af55a3dd929cd865d824a4e
Francis <francis.yanga@gmail.com>
2014-12-22 10:46:15 +1300

    initial upload

---
+++ smtpmailer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace smtpmailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage message = new MailMessage("info@filgifts.com", "paz.astadan@gmail.com");
+            message.Subject = "smtp using .net";
+            message.Body = "how cool is this!";
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "email-smtp.us-west-2.amazonaws.com";
+            smtp.Credentials = new NetworkCredential("AKIAIHZXXEETLVFJMLBQ", "Aj5giKnRAGdMQyLIUBgWoxjuG8vXTNy5a+KMBb7WAJpR");
+            smtp.EnableSsl = true;
+            smtp.Send(message);
+        }
+    }
+}
